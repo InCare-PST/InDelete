f9dae81102cedd40be45cfda9639e36e5aa258f7
JT Gallups <justin@gallups.net>
2019-03-27 09:06:23 -0500

    Added CategoryType

--- PS Scripts/Set-FirewallPrivate.ps1
+++ PS Scripts/Set-FirewallPrivate.ps1
@@ -4,8 +4,12 @@ Ideas for furture use:
 1) Delete all profiles and recreate the one that is currently being used, then set it to private.
 
 #>
-
+$alpha = @()
+$bravo = @()
 $alpha = (Get-childitem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles" | Get-ItemProperty -Name Category | Where Category -eq 0).pspath
-$bravo += $alpha.trim("Microsoft.PowerShell.Core\")
+$bravo += $alpha.trim("Microsoft.PowerShell.Core\")|select-object -first 2
 
-foreach($b in $bravo){set-itemproperty -path $b -Name Category -Value 1 -ErrorAction SilentlyContinue}
+foreach($b in $bravo){
+set-itemproperty -path $b -Name Category -Value 1 -ErrorAction SilentlyContinue
+set-itemproperty -path $b -Name CategoryType -Value 0 -ErrorAction SilentlyContinue
+}
