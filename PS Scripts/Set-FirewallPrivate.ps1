da104d201ef0f98bb650fe30d3dc0f019bbe3dfb
JT Gallups <justin@gallups.net>
2019-03-27 10:01:19 -0500

    Add reboot box

--- PS Scripts/Set-FirewallPrivate.ps1
+++ PS Scripts/Set-FirewallPrivate.ps1
@@ -6,6 +6,11 @@ Ideas for furture use:
 #>
 function Set-FirewallPrivate{
 
+  [cmdletbinding()]
+  param(
+      [switch]$NoRestart
+    )
+
 $alpha = @()
 $bravo = @()
 $alpha = (Get-childitem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles" | Get-ItemProperty -Name Category | Where Category -eq 0).pspath
@@ -14,7 +19,28 @@ $bravo += $alpha.trim("Microsoft.PowerShell.Core\")
 foreach($b in $bravo){
 set-itemproperty -path $b -Name Category -Value 1 -ErrorAction SilentlyContinue
 set-itemproperty -path $b -Name CategoryType -Value 0 -ErrorAction SilentlyContinue
-}
+write-host -Foregroundcolor Green (($(get-itemproperty -path $b -Name ProfileName).profilename) + ": has been updated"
 }
 
+if(!$NoRestart){
+    Add-Type -AssemblyName PresentationCore,PresentationFramework
+    $ButtonType = [System.Windows.MessageBoxButton]::YesNo
+    $MessageIcon = [System.Windows.MessageBoxImage]::Exclamation
+    $MessageBody = "This script requires a reboot, would you like to reboot now?"
+    $MessageTitle = "Confirm Reboot"
+
+    $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+    switch ($Result){
+    "Yes" {
+            write-host -ForegroundColor Green"Windows will restart in 30 seconds"
+            start-sleep 30
+            restart-computer -force
+     }
+    "No" {
+    write-host -ForegroundColor Red "Please restart your computer later"
+
+    }
+  }
+}
+}
 Set-FirewallPrivate
