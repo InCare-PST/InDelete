5530d9ac021b24fd67c4f23ffe165bb6c23b227b
JT Gallups <justin@gallups.net>
2019-03-27 09:17:14 -0500

    Whooops

--- PS Scripts/Set-FirewallPrivate.ps1
+++ PS Scripts/Set-FirewallPrivate.ps1
@@ -4,12 +4,17 @@ Ideas for furture use:
 1) Delete all profiles and recreate the one that is currently being used, then set it to private.
 
 #>
+function Set-FirewallPrivate{
+
 $alpha = @()
 $bravo = @()
 $alpha = (Get-childitem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles" | Get-ItemProperty -Name Category | Where Category -eq 0).pspath
-$bravo += $alpha.trim("Microsoft.PowerShell.Core\")|select-object -first 2
+$bravo += $alpha.trim("Microsoft.PowerShell.Core\")
 
 foreach($b in $bravo){
 set-itemproperty -path $b -Name Category -Value 1 -ErrorAction SilentlyContinue
 set-itemproperty -path $b -Name CategoryType -Value 0 -ErrorAction SilentlyContinue
 }
+}
+
+Set-FirewallPrivate
