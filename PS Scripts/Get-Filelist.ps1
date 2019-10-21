6227f0d3fd1a55f461cdfbb7e92a237a249ecb38
Drethos <47424284+Drethos@users.noreply.github.com>
2019-10-21 11:39:42 -0500

    workflow tests

--- PS Scripts/Get-Filelist.ps1
+++ PS Scripts/Get-Filelist.ps1
@@ -1,4 +1,19 @@
-﻿$files = Get-ChildItem -Path C:\temp -File -Recurse
+﻿<#
+Find the username for the person logging in the most over the past 7 days.
+#>
+
+$startDate = (Get-Date) - (New-TimeSpan -Day 5)
+$UserLoginTypes = 2,7
+<#
+Clean this up
+#>
+$user = Get-WinEvent  -FilterHashtable @{Logname='Security';ID=4624;StartTime=$startDate}  | SELECT TimeCreated, @{N='Username'; E={$_.Properties[5].Value}}, @{N='LogonType'; E={$_.Properties[8].Value}} | WHERE {$UserLoginTypes -contains $_.LogonType}  | Sort-Object count | Select -last 1
+
+
+<#
+Get a list of files from the C:\Users\%username% directory
+#>
+$files = Get-ChildItem -Path C:\temp -File -Recurse
 foreach ($file in $files){
     $extension = $file.extension.split(".")[1]
     $varname = $extension
@@ -22,6 +37,3 @@ foreach($fextension in $fextensions){
 
 
 
-$startDate = (Get-Date) - (New-TimeSpan -Day 5)
-$UserLoginTypes = 2,7
-$user = Get-WinEvent  -FilterHashtable @{Logname='Security';ID=4624;StartTime=$startDate}  | SELECT TimeCreated, @{N='Username'; E={$_.Properties[5].Value}}, @{N='LogonType'; E={$_.Properties[8].Value}} | WHERE {$UserLoginTypes -contains $_.LogonType}  | Sort-Object count | Select -last 1
